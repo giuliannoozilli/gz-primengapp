3a39c8a8579d846d9b2501f618f92139954486fe
Giulianno Zilli <gzilli@hotmail.com>
2021-06-30 11:23:36 -0300

    rpj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './componentes/calendar/calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cal', component: CalendarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {CalendarModule} from 'primeng/calendar';
+import { CalendarComponent } from './componentes/calendar/calendar.component';
+import {ButtonModule} from 'primeng/button';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalendarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
